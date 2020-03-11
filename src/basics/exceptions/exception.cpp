f6d9191fa56071f4f18d5ca7eb1effadc02e343b
mui <mui@one.com>
2020-03-11 20:43:06 +0400

    Learned about invariants

--- src/basics/exceptions/exception.cpp
+++ src/basics/exceptions/exception.cpp
@@ -2,48 +2,85 @@
 
 using namespace std;
 
-class Vec {
-    public:
-        Vec(int s) :sz{s}, elem{new double[s]} {}
-        double& operator[](int i) 
-        { 
-            if (i < 0 || size() <= i) 
-            {
-                throw out_of_range{"Vec::operator[]"};
-            }
-            return elem[i]; 
+class Vec
+{
+public:
+    Vec()
+    {
+        sz = 0;
+        elem = nullptr;
+    }
+    Vec(int s)
+    {
+        if (s < 0)
+            throw length_error{"invalid length for the vector"};
+        sz = s;
+        elem = new double[s];
+    }
+
+    ~Vec()
+    {
+        delete[] elem;
+    }
+
+    double &operator[](int i)
+    {
+        if (i < 0 || size() <= i)
+        {
+            throw out_of_range{"Vec::operator[]"};
         }
-        int size() { return sz; }
-    private:
-        int sz;
-        double* elem;
+        return elem[i];
+    }
+    int size() { return sz; }
+
+private:
+    int sz;
+    double *elem;
 };
 
-int main()
+Vec get_vec()
 {
     int size;
     cout << "Give the size for your vector\n";
     cin >> size;
-
-    Vec v(size);
-    cout << "Vec is of size: " << v.size() << "\n";
-    cout << "Enter " << v.size() << " numbers\n";
-    for (int i = 0; i < v.size(); i++)
-        cin >> v[i];
-    
-    int index = 0;
-
-    for (int i = 0; i < v.size(); i++)
+    Vec v;
+    try
     {
-        cout << "Provide the index of vector to see what you got there \n";
-        cin >> index;
-        try {
-            double value = v[index];
-            cout << "You got " << value << " at index " << index << " of vector \n";
-        } 
-        catch (out_of_range)
+        v = Vec(size);
+    }
+    catch (length_error)
+    {
+        cout << "Please provide a valid length anything above -1\ntry again\n";
+        return get_vec();
+    }
+    return v;
+}
+
+int main()
+{
+    Vec v = get_vec();
+    cout << "Vec is of size: " << v.size() << "\n";
+    if (v.size() > 0)
+    {
+        cout << "Enter " << v.size() << " numbers\n";
+        for (int i = 0; i < v.size(); i++)
+            cin >> v[i];
+
+        int index = 0;
+
+        for (int i = 0; i < v.size(); i++)
         {
-            cout << "You provided incorrect index " << index << "\n";
+            cout << "Provide the index of vector to see what you got there \n";
+            cin >> index;
+            try
+            {
+                double value = v[index];
+                cout << "You got " << value << " at index " << index << " of vector \n";
+            }
+            catch (out_of_range)
+            {
+                cout << "You provided incorrect index " << index << "\n";
+            }
         }
     }
 
